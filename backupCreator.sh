162b15db2f2c700e3398eeed56c19a2a37e5adb0
dementisimus <54750441+dementisimus@users.noreply.github.com>
2020-05-03 22:42:52 +0200

    Add deletion and re-creation of log-file

--- backupCreator.sh
+++ backupCreator.sh
@@ -7,6 +7,7 @@ loc_3="/var/www/"
 loc_4="/var/lib/mysql/"
 
 buDest="/.backupService/Backups/"
+loDest="/.backupService/logs/"
 
 timestamp=$(date -d "today" +"%d.%m.%Y-%H,%M,%S")
 archive_file="$timestamp.zip"
@@ -19,6 +20,12 @@ cd $buDest
 
 # "3" can be changed to the amount of backups you want to keep as a maximum
 find $buDest -mtime +3 -type f -delete
+
+cd $loDest
+
+# removes backup logs and re-creates the file
+rm "/.backupService/logs/logs.log"
+touch "/.backupService/logs/logs.log"
 
 # executes automatic updates
 apt-get update -y && apt-get upgrade -y
